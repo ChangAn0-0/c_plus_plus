a52e72c39a6b5f8086d20de86b7f657a26343b48
ChangAn <1533167307@qq.com>
2022-10-25 18:07:20 +0800

    0-0

---
+++ ChapterThree/test_1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+int main(void)
+{
+	using namespace std;
+	int num = 16;
+	int num2{ 32 };
+	cout << num << endl;
+	cout << hex;
+	cout << num << endl;
+	cout << oct;
+	cout << num << endl;
+	cout << num2 << endl;
+	return 0;
+}
