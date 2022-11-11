4beb6687f548bebb8b5b3a98da0ce8edab727fdb
ChangAn <1533167307@qq.com>
2022-11-11 20:42:08 +0800

    0-0

---
+++ ChaptorThirteen/brass.h
@@ -0,0 +1,18 @@
+#ifndef BRASS_H_
+#define BRASS_H_
+#include <string>
+
+class Brass
+{
+private:
+	std::string fullName;
+	long acctNum;
+	double balance;
+public:
+	Brass(const std::string& s = "NUllbody", long an = -1, double bal = 0.0);
+	void Deposit(double amt);
+	virtual void Withdraw(double amt);
+	double
+};
+#endif // !1
+
