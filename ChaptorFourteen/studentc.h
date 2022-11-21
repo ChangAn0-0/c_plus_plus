9e0edf9528bab6fc19fcd23c3cdf1bf36a4fae95
ChangAn <1533167307@qq.com>
2022-11-21 21:10:34 +0800

    0-0

---
+++ ChaptorFourteen/studentc.h
@@ -0,0 +1,19 @@
+#ifndef STUDENTC_H_
+#define STUDENTC_H_
+
+#include <iostream>
+#include <string>
+#include <valarray>
+class Student
+{
+private:
+	typedef std::valarray<double> ArrayDb;
+	std::string name;
+	ArrayDb scores;
+	std::ostream& arr_out(std::ostream& os) const;
+public:
+	Student():name("Null Student"),scores() {}
+	explicit Student(const std::string & s)
+		:name(s),scores() {}
+};
+#endif
