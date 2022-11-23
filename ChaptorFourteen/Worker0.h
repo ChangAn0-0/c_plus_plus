e4a9edfc11252e2ca87ea3bf4b4c02e22337ab8c
ChangAn <1533167307@qq.com>
2022-11-23 18:31:08 +0800

    0-0

---
+++ ChaptorFourteen/Worker0.h
@@ -0,0 +1,16 @@
+#ifndef WORKER0_H_	
+#define WORKER0_H_
+
+#include <string>
+
+class Worker
+{
+private:
+	std::string fullname;
+	long id;
+public:
+	Worker():fullname("no one"),id(0L) {}
+
+};
+
+#endif
