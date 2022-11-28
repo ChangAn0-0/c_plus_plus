240850fc9ca8ad3634e592b59f9f007f8e5cfcc9
ChangAn <1533167307@qq.com>
2022-11-28 22:18:44 +0800

    0-0

--- ChaptorFiveteen/tv.h
+++ ChaptorFiveteen/tv.h
@@ -1,1 +1,19 @@
-#pragma once
+#ifndef TV_H_
+#define TV_H_
+
+class Tv
+{
+public:
+	friend class Remote;
+	enum{Off,On};
+	enum{MinVal,MaxVal=20};
+	enum{Antenna,Cable};
+	enum{TV,DVD};
+
+	Tv(int s=Off,int mc=125):state(s),volume(5),maxchannel(mc),channel(2),mode(Caable),input(TV){} 
+	void onoff() { state = (state == On) ? Off : On; }
+	bool ison() const { return == On; }
+	bool voiup
+};
+#endif // !TV_H_
+
